43ea8405fbdad02866106fa6e05b748bde13dec4
Fadil p <76585854+fadilniju@users.noreply.github.com>
2025-07-18 16:00:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        // This allows Next.js to optimize images from the specified domains.
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.pollinations.ai',
+                port: '',
+                pathname: '/prompt/**',
+            },
+            {
+                // This is for the fallback developer avatar I added.
+                protocol: 'https://',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            }
+        ],
+    },
+};
 
 export default nextConfig;
